5436cc8873bb5ff648086a4e0493431f5635c066
Kevin McFadden <kevin@conceptsahead.com>
2009-03-30 11:41:22 -0600

    Initial simple-gem files.

---
+++ test/unit/amiruby19_test.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class Amiruby19Test < Test::Unit::TestCase
+
+  describe "An instance of the Amiruby19 class" do
+
+    it "should flunk" do
+      flunk "Please provide some tests"
+    end
+    
+  end
+
+end
