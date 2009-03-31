8e012f77086fdebafc76f4eb5426c516b6f6541c
Kevin McFadden <kevin@conceptsahead.com>
2009-03-30 21:36:54 -0600

    provide status information while running.

--- lib/amiruby19/amiruby19.rb
+++ lib/amiruby19/amiruby19.rb
@@ -1,3 +1,6 @@
+#
+# Yes, this script is a quick utility hack.
+#
 require 'rubygems'
 require 'json'
 require 'open-uri'
@@ -12,24 +15,30 @@ isit19_url = "http://isitruby19.com"
 isit19_file = "comments.json"
 
 installed_gems = %x[gem list].split("\n")
+
+puts "Found #{installed_gems.size} installed gem(s)."
+
 installed_gems.each do |g|
+  $stdout.print "."
+  $stdout.flush
+
   gem_name = g.split(" ")[0]
   url = isit19_url + "/" + gem_name + "/" + isit19_file
 
   begin
-    doc = open(url).read
+    json_data = open(url).read
   rescue
     missing << gem_name
     next
   end
 
-  json = JSON.parse(doc)
+  gem_comments = JSON.parse(json_data)
 
-  if json.size == 0
+  if gem_comments.size == 0
     untested << gem_name
   else
     sum = 0
-    recent = json[0..4]
+    recent = gem_comments[0..4]
 
     recent.each do |r|
       if r["comment"]["works_for_me"]
@@ -50,17 +59,19 @@ installed_gems.each do |g|
   end
 end
 
-puts "WORKS\n-----"
+puts "\nFinished!\n\n"
+
+puts "WORKS (#{works.size})\n-----"
 puts works.keys.join(" ")
 
-puts "\nMAY WORK\n--------"
+puts "\nMAY WORK (#{may_work.size})\n--------"
 puts may_work.keys.join(" ")
 
-puts "\nFAILS\n-----"
+puts "\nFAILS (#{fails.size})\n-----"
 puts fails.keys.join(" ")
 
-puts "\nUNTESTED\n--------"
+puts "\nUNTESTED (#{untested.size})\n--------"
 puts untested.join(" ")
 
-puts "\nMISSING\n-------"
+puts "\nMISSING (#{missing.size})\n-------"
 puts missing.join(" ")
